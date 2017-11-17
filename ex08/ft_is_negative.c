bbeb810106b5d6124b29872ec04ec403fbb8c003
Cyril LEMAIRE <clemaire@student.42.fr>
2017-11-17 17:09:01 +0100

    Corrected ex08

--- ex08/ft_is_negative.c
+++ ex08/ft_is_negative.c
@@ -6,14 +6,13 @@
 /*   By: clemaire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 10:25:26 by clemaire          #+#    #+#             */
-/*   Updated: 2017/11/13 15:30:12 by clemaire         ###   ########.fr       */
+/*   Updated: 2017/11/17 15:43:30 by clemaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_putchar(int c);
 
-int		ft_is_negative(int n)
+void	ft_is_negative(int n)
 {
 	ft_putchar(n < 0 ? 'N' : 'P');
-	ft_putchar('\n');
 }
